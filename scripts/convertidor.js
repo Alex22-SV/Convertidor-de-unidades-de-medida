a6393599c3c9b6df59b657c22527faf1a056322d
Alex0622 <aleximora2206@gmail.com>
2022-02-16 11:23:27 -0600

    Agregar unidades de tiempo

--- scripts/convertidor.js
+++ scripts/convertidor.js
@@ -10,6 +10,12 @@ const optionsmasa2 = document.getElementById("options-masa-2");
 const formmasa = document.getElementById('form-masa');
 const optionnumbermasa = document.getElementById("option-number-masa");
 const displayResultmasa = document.getElementById('displayResult-masa');
+//TIEMPO
+const optionstiempo1 = document.getElementById("options-tiempo-1");
+const optionstiempo2 = document.getElementById("options-tiempo-2");
+const formtiempo = document.getElementById('form-tiempo');
+const optionnumbertiempo = document.getElementById("option-number-tiempo");
+const displayResulttiempo = document.getElementById('displayResult-tiempo');
 
 formlongitud.addEventListener('submit', e => {
     e.preventDefault()
@@ -18,7 +24,6 @@ formlongitud.addEventListener('submit', e => {
     const number = optionnumberlongitud.value
     const result = convertirResultado(option1, number, option2)
     displayResultlongitud.innerText = result
-    return
 })
 formmasa.addEventListener('submit', e => {
     e.preventDefault()
@@ -27,7 +32,14 @@ formmasa.addEventListener('submit', e => {
     const number = optionnumbermasa.value
     const result = convertirResultado(option1, number, option2)
     displayResultmasa.innerText = result
-    return
+})
+formtiempo.addEventListener('submit', e => {
+    e.preventDefault()
+    const option1 = optionstiempo1.value
+    const option2 = optionstiempo2.value
+    const number = optionnumbertiempo.value
+    const result = convertirResultado(option1, number, option2)
+    displayResulttiempo.innerText = result
 })
 
 function convertirResultado(option1, number, option2) {
@@ -283,4 +295,325 @@ function convertirResultado(option1, number, option2) {
             return `${number}µg`
         }
     }
+    /* Unidad de tiempo */
+    if(option1 == "milenio") {
+        if(option2 == "milenio") {
+            return `${number} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number*10} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number*100} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number*1000} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number*12000} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number*52143} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number*365000}d`
+        }
+        if(option2 == "h") {
+            return `${number*8760000}h`
+        }
+        if(option2 == "min") {
+            return `${number*525600000}min`
+        }
+        if(option2 == "s") {
+            return `${number*31540000000}s`
+        }
+    }
+    if(option1 == "siglo") {
+        if(option2 == "milenio") {
+            return `${number/10} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number*10} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number*100} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number*1200} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number*5214} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number*36500}d`
+        }
+        if(option2 == "h") {
+            return `${number*876000}h`
+        }
+        if(option2 == "min") {
+            return `${number*52560000}min`
+        }
+        if(option2 == "s") {
+            return `${number*3154000000}s`
+        }
+    }
+    if(option1 == "decada") {
+        if(option2 == "milenio") {
+            return `${number/100} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/10} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number*10} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number*120} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number*521} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number*3650}d`
+        }
+        if(option2 == "h") {
+            return `${number*87600}h`
+        }
+        if(option2 == "min") {
+            return `${number*5256000}min`
+        }
+        if(option2 == "s") {
+            return `${number*315400000}s`
+        }
+    }
+    if(option1 == "año") {
+        if(option2 == "milenio") {
+            return `${number/1000} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/100} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/10} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number*12} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number*52.143} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number*365}d`
+        }
+        if(option2 == "h") {
+            return `${number*8760}h`
+        }
+        if(option2 == "min") {
+            return `${number*525600}min`
+        }
+        if(option2 == "s") {
+            return `${number*31540000}s`
+        }
+    }
+    if(option1 == "mes") {
+        if(option2 == "milenio") {
+            return `${number/12000} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/1200} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/120} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number/12} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number*4.345} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number*30.417}d`
+        }
+        if(option2 == "h") {
+            return `${number*730}h`
+        }
+        if(option2 == "min") {
+            return `${number*43800}min`
+        }
+        if(option2 == "s") {
+            return `${number*2628000}s`
+        }
+    }
+    if(option1 == "semana") {
+        if(option2 == "milenio") {
+            return `${number/52143} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/5214} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/521} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number/52.143} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number/4.345} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number*7}d`
+        }
+        if(option2 == "h") {
+            return `${number*168}h`
+        }
+        if(option2 == "min") {
+            return `${number*10080}min`
+        }
+        if(option2 == "s") {
+            return `${number*604800}s`
+        }
+    }
+    if(option1 == "d") {
+        if(option2 == "milenio") {
+            return `${number/365000} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/36500} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/3650} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number/365} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number/30.417} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number/7} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number}d`
+        }
+        if(option2 == "h") {
+            return `${number*24}h`
+        }
+        if(option2 == "min") {
+            return `${number*1440}min`
+        }
+        if(option2 == "s") {
+            return `${number*86400}s`
+        }
+    }
+    if(option1 == "h") {
+        if(option2 == "milenio") {
+            return `${number/8.76e+6} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/876000} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/87600} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number/8760} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number/730} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number/168} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number/24}d`
+        }
+        if(option2 == "h") {
+            return `${number}h`
+        }
+        if(option2 == "min") {
+            return `${number*60}min`
+        }
+        if(option2 == "s") {
+            return `${number*3600}s`
+        }
+    }
+    if(option1 == "min") {
+        if(option2 == "milenio") {
+            return `${number/5.256e+8} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/5.256e+7} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/5.256e+6} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number/525600} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number/43800} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number/10080} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number/1440}d`
+        }
+        if(option2 == "h") {
+            return `${number/60}h`
+        }
+        if(option2 == "min") {
+            return `${number}min`
+        }
+        if(option2 == "s") {
+            return `${number*60}s`
+        }
+    }
+    if(option1 == "s") {
+        if(option2 == "milenio") {
+            return `${number/3.154e+10} milenio(s)`
+        }
+        if(option2 == "siglo") {
+            return `${number/3.154e+9} siglo(s)`
+        }
+        if(option2 == "decada") {
+            return `${number/3.154e+8} década(s)`
+        }
+        if(option2 == "año") {
+            return `${number/3.154e+7} año(s)`
+        }
+        if(option2 == "mes") {
+            return `${number/2.628e+6} mes(es)`
+        }
+        if(option2 == "semana") {
+            return `${number/604800} semana(s)`
+        }
+        if(option2 == "d") {
+            return `${number/86400}d`
+        }
+        if(option2 == "h") {
+            return `${number/3600}h`
+        }
+        if(option2 == "min") {
+            return `${number/60}min`
+        }
+        if(option2 == "s") {
+            return `${number}s`
+        }
+    }   
 }
